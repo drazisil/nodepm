af12a657a2a312246e75fee697d57bfd5c22f09a
Drazi Crendraven <drazisil@users.noreply.github.com>
2022-05-23 22:11:26 -0400

    add comments

--- src/cli.rs
+++ src/cli.rs
@@ -1,37 +1,24 @@
-use clap::{ Args, Parser, Subcommand };
+use clap::{Args, Parser, Subcommand};
 use clap_verbosity_flag::Verbosity;
 
+// Top level clap::Command
 #[derive(Parser)]
 #[structopt(name = "nodepm")]
 pub struct Cli {
     // Subcommands
+    // You can only have one subcommand section
+    // so we point this to the Commands struct
     #[structopt(subcommand)]
     pub command: Commands,
 
     #[structopt(flatten)]
-    verbose: Verbosity
+    verbose: Verbosity,
 }
 
-#[derive(Parser)]
-pub struct Project {
-         #[structopt(subcommand)]
-        pub project_commands: ProjectCommands,
-}
-
-#[derive(Parser)]
-pub struct Package {
-         #[structopt(subcommand)]
-        pub package_commands: PackageCommands,
-}
-
-#[derive(Args)]
-pub struct InspectArgs {
-    pub project_name: String,
-
-    /// Package version [default: 'latest']
-    pub version: String,
-}
-
+// Second tier of commands -  clap::Subcommand
+// Normally this would point to a clap::Args object,
+// but in this case we are passing in another clap::Subcommand
+// This enum defines our subcommand groups and passes on down
 #[derive(Subcommand)]
 pub enum Commands {
     /// Project commands
@@ -42,31 +29,67 @@ pub enum Commands {
     Package(Package),
 }
 
-#[derive(Subcommand)]
-pub enum ProjectCommands {
-    /// Initialize a new project (use "init --force" to overwrite an existing one)
-    Init(ProjectInitArgs)
+// The 'project' command itself
+// The only way this differs from the Commands struct
+// is that it directes clap::Parser, instead of clap::Subcommand
+// This is so it gains the ability to auto-parse into
+// the structure tree as a clap::Command with args, instead of as
+// a clap::Args object
+#[derive(Parser)]
+pub struct Project {
+    #[structopt(subcommand)]
+    pub project_commands: ProjectCommands,
 }
 
-#[derive(Subcommand)]
-pub enum PackageCommands {
-    /// Inspect a package
-    Inspect(PackageInspectArgs)
+// The 'package' command itself
+// The only way this differs from the Commands struct
+// is that it directes clap::Parser, instead of clap::Subcommand
+// This is so it gains the ability to auto-parse into
+// the structure tree as a clap::Command with args, instead of as
+// a clap::Args object
+#[derive(Parser)]
+pub struct Package {
+    #[structopt(subcommand)]
+    pub package_commands: PackageCommands,
 }
 
 #[derive(Args)]
+pub struct InspectArgs {
+    pub project_name: String,
+
+    /// Package version [default: 'latest']
+    pub version: String,
+}
+
+// The command enum for the 'project' command
+// Here we pass in the args for the 'project init' command
+#[derive(Subcommand)]
+pub enum ProjectCommands {
+    /// Initialize a new project (use "init --force" to overwrite an existing one)
+    Init(ProjectInitArgs),
+}
+
+// The command enum for the 'package' command
+// Here we pass in the args for the 'package inspect' command
+#[derive(Subcommand)]
+pub enum PackageCommands {
+    /// Inspect a package
+    Inspect(PackageInspectArgs),
+}
+
+// The final clap::Args struct for the project init command
+#[derive(Args)]
 pub struct ProjectInitArgs {
-        /// Overwrite an existing project
-        #[clap(long = "force")]
-        pub force: bool,
+    /// Overwrite an existing project
+    #[clap(long = "force")]
+    pub force: bool,
 
-        /// The name to add to the package.json file
-        pub project_name: String,
+    /// The name to add to the package.json file
+    pub project_name: String,
 
-        /// The `path` to create a the project in. Defaults to the current directory (`.`)
-        #[clap(default_value = ".")]
-        pub path: std::path::PathBuf,
-
+    /// The `path` to create a the project in. Defaults to the current directory (`.`)
+    #[clap(default_value = ".")]
+    pub path: std::path::PathBuf,
 }
 
 #[derive(Args)]
